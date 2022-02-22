78be9255e390a0e1d700f55e4a46cb8e5d15c074
潘守恒 <shouheng.pan@tineco.com>
2022-02-22 11:20:31 +0800

    Initial Commit

---
+++ TinecoTrackDataUITests/TinecoTrackDataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TinecoTrackDataUITestsLaunchTests.swift
+//  TinecoTrackDataUITests
+//
+//  Created by psh on 2022/2/22.
+//
+
+import XCTest
+
+class TinecoTrackDataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
